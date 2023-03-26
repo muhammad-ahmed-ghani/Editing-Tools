e5615d8605ef4e18d9b027f63851c560845c6d33
Muhammad Ahmed <i.am.a.pakistani.programmer@gmail.com>
2023-03-26 18:12:33 +0500

    Update image_variations.py

--- image_variations.py
+++ image_variations.py
@@ -1,7 +1,7 @@
 import os
+import cv2
 from PIL import Image
-import kornia as K
-from kornia.core import Tensor
+import numpy as np
 from diffusers import AutoencoderKL
 from diffusers import UniPCMultistepScheduler
 from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
@@ -13,12 +13,13 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 # Blip for Image Captioning
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained(
-            "Salesforce/blip-image-captioning-base", torch_dtype=torch.float16).to(device)
+            "Salesforce/blip-image-captioning-base", 
+            torch_dtype=torch.float16).to(device)
 
 # Promptist for Prompt Enhancement
-prompter_model = AutoModelForCausalLM.from_pretrained("microsoft/Promptist")
+prompter_model = AutoModelForCausalLM.from_pretrained("microsoft/Promptist", device_map='auto', load_in_8bit=True)
 prompter_tokenizer  = AutoTokenizer.from_pretrained("gpt2")
-promptist_pipeline = pipeline("text-generation", model=prompter_model, tokenizer=prompter_tokenizer, device=torch.device(device))
+promptist_pipeline = pipeline("text-generation", model=prompter_model, tokenizer=prompter_tokenizer)
 
 # ControlNet for Image Variation Generation based on Canny Edge Detection
 controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16)
@@ -34,37 +35,34 @@ pipe = StableDiffusionControlNetPipeline.from_pretrained(
 ).to(device)
 
 pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
-pipe.enable_model_cpu_offload()
 pipe.enable_xformers_memory_efficient_attention()
 
-os.makedirs("variations", exist_ok=True)
+def pre_process_image(image):
+  image = np.array(image)
+  low_threshold = 100
+  high_threshold = 200
+  image = cv2.Canny(image, low_threshold, high_threshold)
+  image = image[:, :, None]
+  image = np.concatenate([image, image, image], axis=2)
+  return Image.fromarray(image)
 
 def image_variations(image, input_prompt):
-    image.save("variations/input.png")
-    img: Tensor = K.io.load_image("variations/input.png", K.io.ImageLoadType.RGB32)    
-    img = img[None]
-    x_gray = K.color.rgb_to_grayscale(img)     
-    x_canny: Tensor = K.filters.canny(x_gray)[0]
-    canny_image = K.utils.tensor_to_image(1. - x_canny.clamp(0., 1.0))
-    canny_image = Image.fromarray((canny_image * 255).astype('uint8'))
-    canny_image = canny_image.convert("L")
-    canny_image = canny_image.point(lambda x: 0 if x > 128 else 255, mode="1")
-    canny_image = canny_image.convert("RGB")
-
+    canny_image = pre_process_image(image)
     if input_prompt:
         prompt = input_prompt
     else:
         inputs = processor(image, return_tensors="pt").to(device, torch.float16)
         out = model.generate(**inputs)
         plain_text = processor.decode(out[0], skip_special_tokens=True)
+        print(f"Blip Captioning: {plain_text}")
         prompt = promptist_pipeline(plain_text)[0]["generated_text"]
         print(f"Reconstructed prompt: {prompt}")
         
     output_images = pipe(
-        [prompt]*4,
+        [prompt]*2,
         canny_image,
-        negative_prompt=["distorted, noisy, lowres, bad anatomy, worst quality, low quality, bad eyes"] * 4,
+        negative_prompt=["distorted, noisy, lowres, bad anatomy, worst quality, low quality, bad eyes, rough face, unclear face"] * 2,
         num_inference_steps=25,
     ).images
 
-    return output_images
+    return output_images, canny_image
