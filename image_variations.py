df0a4c9fec4172014a28ca67ccf4ff9223761e4d
Muhammad Ahmed <i.am.a.pakistani.programmer@gmail.com>
2023-03-26 19:32:24 +0500

    Update image_variations.py

--- image_variations.py
+++ image_variations.py
@@ -6,7 +6,7 @@ from diffusers import AutoencoderKL
 from diffusers import UniPCMultistepScheduler
 from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
 import torch
-from transformers import BlipProcessor, BlipForConditionalGeneration, AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import BlipProcessor, BlipForConditionalGeneration
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -15,11 +15,6 @@ processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base
 model = BlipForConditionalGeneration.from_pretrained(
             "Salesforce/blip-image-captioning-base", 
             torch_dtype=torch.float16).to(device)
-
-# Promptist for Prompt Enhancement
-prompter_model = AutoModelForCausalLM.from_pretrained("microsoft/Promptist", device_map='auto', load_in_8bit=True)
-prompter_tokenizer  = AutoTokenizer.from_pretrained("gpt2")
-promptist_pipeline = pipeline("text-generation", model=prompter_model, tokenizer=prompter_tokenizer)
 
 # ControlNet for Image Variation Generation based on Canny Edge Detection
 pipe = StableDiffusionControlNetPipeline.from_pretrained(
@@ -54,15 +49,13 @@ def image_variations(image, input_prompt):
     else:
         inputs = processor(image, return_tensors="pt").to(device, torch.float16)
         out = model.generate(**inputs)
-        plain_text = processor.decode(out[0], skip_special_tokens=True)
-        print(f"Blip Captioning: {plain_text}")
-        prompt = promptist_pipeline(plain_text)[0]["generated_text"]
-        print(f"Reconstructed prompt: {prompt}")
+        prompt = processor.decode(out[0], skip_special_tokens=True)
+        print(f"Blip Captioning: {prompt}")
         
     output_images = pipe(
-        [prompt]*2,
+        [prompt]*4,
         canny_image,
-        negative_prompt=["distorted, noisy, lowres, bad anatomy, worst quality, low quality, bad eyes, rough face, unclear face"] * 2,
+        negative_prompt=["distorted, noisy, lowres, bad anatomy, worst quality, low quality, bad eyes, rough face, unclear face"] * 4,
         num_inference_steps=25,
     ).images
 
