3f918020b9ef2fffedcbb9af8a2ee5b630a1b656
Muhammad Ahmed <i.am.a.pakistani.programmer@gmail.com>
2023-07-17 11:03:42 +0500

    Update image_inpainting.py

--- image_inpainting.py
+++ image_inpainting.py
@@ -19,14 +19,16 @@ pipe.enable_xformers_memory_efficient_attention()
 os.makedirs("inpainting_output", exist_ok=True)
 
 def inpaint(inputs, prompt):
-    image = inputs["image"].resize((image.size[0] - image.size[0] % 64, image.size[1] - image.size[1] % 64), Image.ANTIALIAS)
-    mask = inputs["mask"].resize((mask.size[0] - mask.size[0] % 64, mask.size[1] - mask.size[1] % 64), Image.ANTIALIAS)
     # Resize them to have less then 1024 pixels on the largest side by keeping the aspect ratio
     max_size = 1024
     if max(image.size) > max_size:
         image.thumbnail((max_size, max_size), Image.ANTIALIAS)
     if max(mask.size) > max_size:
         mask.thumbnail((max_size, max_size), Image.ANTIALIAS)
+        
+    image = inputs["image"].resize((image.size[0] - image.size[0] % 64, image.size[1] - image.size[1] % 64), Image.ANTIALIAS)
+    mask = inputs["mask"].resize((mask.size[0] - mask.size[0] % 64, mask.size[1] - mask.size[1] % 64), Image.ANTIALIAS)
+    
     output = pipe(prompt=prompt, image=image, mask_image=mask, guidance_scale=7.5, height=image.size[1], width=image.size[0])
     output.images[0].save(f"inpainting_output/output.png")
     return output.images[0], "inpainting_output/output.png"
